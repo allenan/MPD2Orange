3c60d8c80e0ce885559686c9bc2b0ef7610d5f00
unknown <aaronsteinfeld2012@u.northwestern.edu>
2011-04-09 18:24:36 -0500

    updated db schema

--- mpd2.sql
+++ mpd2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 07, 2011 at 02:57 AM
+-- Generation Time: Apr 10, 2011 at 01:22 AM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -24,6 +24,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Table structure for table `alumni`
 --
+-- Creation: Apr 05, 2011 at 08:20 PM
+--
 
 DROP TABLE IF EXISTS `alumni`;
 CREATE TABLE IF NOT EXISTS `alumni` (
@@ -36,38 +38,28 @@ CREATE TABLE IF NOT EXISTS `alumni` (
   `ProjID` int(4) NOT NULL,
   `AlumniID` int(11) NOT NULL AUTO_INCREMENT,
   `TeamID` int(11) NOT NULL,
+  `info` longtext,
+  `facebook` varchar(64) DEFAULT NULL,
+  `twitter` varchar(64) DEFAULT NULL,
+  `linkedin` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`AlumniID`),
   KEY `UserID` (`UserID`),
   KEY `ProjID` (`ProjID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `alumni`
 --
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bio`
---
-
-DROP TABLE IF EXISTS `bio`;
-CREATE TABLE IF NOT EXISTS `bio` (
-  `AlumniID` int(4) NOT NULL,
-  `Dummy` longtext NOT NULL,
-  PRIMARY KEY (`AlumniID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `bio`
---
-
+INSERT INTO `alumni` (`UserID`, `CurrentEmployer`, `GraduationYear`, `Industry`, `FirstName`, `LastName`, `ProjID`, `AlumniID`, `TeamID`, `info`, `facebook`, `twitter`, `linkedin`) VALUES
+(4, 'Microsoft', 2008, 'CS', 'John', 'Doe', 2, 4, 100, 'I''m an alumni', 'With a facebook', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `email`
+--
+-- Creation: Apr 06, 2011 at 07:38 PM
 --
 
 DROP TABLE IF EXISTS `email`;
@@ -90,13 +82,17 @@ CREATE TABLE IF NOT EXISTS `email` (
 --
 -- Table structure for table `images`
 --
+-- Creation: Apr 06, 2011 at 07:51 PM
+--
 
 DROP TABLE IF EXISTS `images`;
 CREATE TABLE IF NOT EXISTS `images` (
   `ImageID` int(11) NOT NULL,
-  `AlumniID` int(11) NOT NULL,
-  `ProjID` int(11) NOT NULL,
+  `AlumniID` int(11) DEFAULT NULL,
+  `ProjID` int(11) DEFAULT NULL,
   `ImageURL` varchar(256) NOT NULL,
+  `imgType` int(1) NOT NULL,
+  `description` longtext,
   PRIMARY KEY (`ImageID`),
   KEY `ProjID` (`ProjID`),
   KEY `AlumniID` (`AlumniID`)
@@ -106,29 +102,38 @@ CREATE TABLE IF NOT EXISTS `images` (
 -- Dumping data for table `images`
 --
 
+INSERT INTO `images` (`ImageID`, `AlumniID`, `ProjID`, `ImageURL`, `imgType`, `description`) VALUES
+(0, NULL, NULL, 'dummyURL', 1, 'A Cool Img');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `project`
 --
+-- Creation: Apr 06, 2011 at 07:45 PM
+--
 
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE IF NOT EXISTS `project` (
   `ProjID` int(4) NOT NULL AUTO_INCREMENT,
   `ProjectName` varchar(32) NOT NULL,
+  `Summary` longtext,
   PRIMARY KEY (`ProjID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `project`
 --
 
+INSERT INTO `project` (`ProjID`, `ProjectName`, `Summary`) VALUES
+(2, 'Dummy Project', 'Cool Project.');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
+--
+-- Creation: Apr 03, 2011 at 01:43 PM
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -139,12 +144,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Privileges` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `Login` (`Login`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `users`
 --
 
+INSERT INTO `users` (`UserID`, `Login`, `Password`, `Privileges`) VALUES
+(4, '63a9f0ea7bb98050796b649e85481845', 'root', 0);
 
 --
 -- Constraints for dumped tables
@@ -154,25 +161,19 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Constraints for table `alumni`
 --
 ALTER TABLE `alumni`
-  ADD CONSTRAINT `alumni_ibfk_2` FOREIGN KEY (`ProjID`) REFERENCES `project` (`ProjID`),
-  ADD CONSTRAINT `alumni_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
-
---
--- Constraints for table `bio`
---
-ALTER TABLE `bio`
-  ADD CONSTRAINT `bio_ibfk_1` FOREIGN KEY (`AlumniID`) REFERENCES `alumni` (`AlumniID`);
+  ADD CONSTRAINT `alumni_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`),
+  ADD CONSTRAINT `alumni_ibfk_2` FOREIGN KEY (`ProjID`) REFERENCES `project` (`ProjID`);
 
 --
 -- Constraints for table `email`
 --
 ALTER TABLE `email`
-  ADD CONSTRAINT `email_ibfk_2` FOREIGN KEY (`AlumniID`) REFERENCES `alumni` (`AlumniID`),
-  ADD CONSTRAINT `email_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
+  ADD CONSTRAINT `email_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`),
+  ADD CONSTRAINT `email_ibfk_2` FOREIGN KEY (`AlumniID`) REFERENCES `alumni` (`AlumniID`);
 
 --
 -- Constraints for table `images`
 --
 ALTER TABLE `images`
-  ADD CONSTRAINT `images_ibfk_2` FOREIGN KEY (`AlumniID`) REFERENCES `alumni` (`AlumniID`),
-  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`ProjID`) REFERENCES `project` (`ProjID`);
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`ProjID`) REFERENCES `project` (`ProjID`),
+  ADD CONSTRAINT `images_ibfk_2` FOREIGN KEY (`AlumniID`) REFERENCES `alumni` (`AlumniID`);
