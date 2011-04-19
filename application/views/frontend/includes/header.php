d83382ab097262030581f30c73a3d4ed58bc7381
unknown <aaronsteinfeld2012@u.northwestern.edu>
2011-04-19 17:53:31 -0500

    Added login link

--- application/views/frontend/includes/header.php
+++ application/views/frontend/includes/header.php
@@ -1,8 +1,11 @@
 	
 <body>
 	<div id="page_container" class="container_12">
-		<div id="header" class="grid_12">
+		<div id="header" class="grid_8 push_2">
 			<h1>MPD2 Program</h1>
+		</div>
+		<div id="login" class="grid_2 push_2">
+		<a href="<?php echo base_url(); echo index_page(); echo '/';?>login">User Login</a>
 		</div>
 		
 		<div id="site_nav" class ="grid_6 push_3">
